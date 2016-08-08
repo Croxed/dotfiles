b52c3f93b11a65e8f99eeb8d33343b7dff66d21b
Oscar Wennergren <oscar@wennergren.se>
2016-08-08 20:09:23 +0200

    Derp search/replace fuckup

---
+++ install.sh
@@ -0,0 +1,161 @@
+#!/usr/bin/env bash
+#
+# Inspired by https://github.com/holman/dotfiles
+#
+
+IGNORE=(
+    ".git"
+    ".gitignore"
+    ".gitmodules"
+    "README.md"
+    ".DS_Store"
+    "install.sh"
+    "bin"
+)
+
+DOTFILES_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo ''
+
+info () {
+    printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+    printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+    echo ''
+    exit
+}
+
+link_file () {
+    local src=$1 dst=$2
+
+    local overwrite= backup= skip=
+    local action=
+
+    if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+    then
+
+        if [ "$overwrite_all" == "false" ] && \
+               [ "$backup_all" == "false" ] && \
+               [ "$skip_all" == "false" ]
+        then
+
+            local currentSrc="$(readlink $dst)"
+
+            if [ "$currentSrc" == "$src" ]
+            then
+
+                skip=true;
+
+            else
+                user "File already exists: $dst ($(basename "$src")), \
+what do you want to do?\n\
+        [s]kip, [S]kip all, [o]verwrite, \
+[O]verwrite all, [b]ackup, [B]ackup all?"
+
+                read -n 1 action
+
+                case "$action" in
+                    o )
+                        overwrite=true;;
+                    O )
+                        overwrite_all=true;;
+                    b )
+                        backup=true;;
+                    B )
+                        backup_all=true;;
+                    s )
+                        skip=true;;
+                    S )
+                        skip_all=true;;
+                    * )
+                    ;;
+                esac
+
+            fi
+
+        fi
+
+        overwrite=${overwrite:-$overwrite_all}
+        backup=${backup:-$backup_all}
+        skip=${skip:-$skip_all}
+
+        if [ "$overwrite" == "true" ]
+        then
+            rm -rf "$dst"
+            success "removed $dst"
+        fi
+
+        if [ "$backup" == "true" ]
+        then
+            mv "$dst" "${dst}.backup"
+            success "moved $dst to ${dst}.backup"
+        fi
+
+        if [ "$skip" == "true" ]
+        then
+            success "skipped $src"
+        fi
+    fi
+
+    if [ "$skip" != "true" ]  # "false" or empty
+    then
+        ln -s "$1" "$2"
+        success "linked $1 to $2"
+    fi
+}
+
+install_dotfiles () {
+    info 'installing dotfiles'
+
+    local overwrite_all=false backup_all=false skip_all=false
+
+    for src in $(find "$DOTFILES_ROOT" -mindepth 1 -maxdepth 1)
+    do
+        if [[ "${IGNORE[@]}" =~ "$(basename $src)" ]]
+        then
+            continue
+        fi
+        dst="$HOME/.$(basename "$src")"
+        link_file "$src" "$dst"
+    done
+}
+
+install_bin () {
+    info 'installing bin'
+    local overwrite_all=false backup_all=false skip_all=false
+    src="$DOTFILES_ROOT/bin"
+    dst="$HOME/$(basename "$src")"
+    link_file "$src" "$dst"
+}
+
+install_zsh () {
+    info 'installing zsh'
+    local overwrite_all=false backup_all=false skip_all=false
+
+    src="$DOTFILES_ROOT/oh-my-zsh"
+    dst="$HOME/$(basename "$src")"
+    link_file "$src" "$dst"
+
+    src="$DOTFILES_ROOT/oh-my-zsh-custom"
+    dst="$HOME/$(basename "$src")"
+    link_file "$src" "$dst"
+}
+
+install_dotfiles
+echo ''
+install_bin
+echo ''
+install_zsh
+
+echo ''
+echo '  All installed!'
