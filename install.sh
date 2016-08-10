5074af4a8ece3adf02ff50036d8b6e15c09ccf02
Oscar Wennergren <oscar@wennergren.se>
2016-08-10 17:04:37 +0200

    This bunny should be killed.

--- install.sh
+++ install.sh
@@ -217,7 +217,7 @@ install_other () {
     done
 
     ask "Install vim-plug?" Y && info "Installing vim-plug" && curl -fsLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && success "Installed vim-plug"
-    if [ "$platform" == "linux" ]; then
+    if [[ "$platform" == "linux" ]]; then
         ask "Make ZSH the default shell?" Y && info "Making ZSH the default shell" && chsh -s "$(which zsh)"; success "Made ZSH default shell"
     elif [[ "$distro" == "macos" ]]; then
         ask "Make ZSH the default shell?" Y && info "Making ZSH the default shell" && sudo dscl . -create /Users/$USER UserShell $(brew --prefix)/bin/zsh; success "Made ZSH default shell"
@@ -262,7 +262,7 @@ ask "Install bin?" Y && install_bin
 echo ''
 ask "Install other scripts and configs?" Y && install_other
 echo ''
-if [ "platform" == "linux" ]; then
+if [[ "$platform" == "linux" ]]; then
     ask "Install configs for Linux?" Y && os_specific
 fi
 
