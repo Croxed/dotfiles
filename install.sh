2f92e938c7607983923b58337bd7a88026688214
Oscar Wennergren <oscar@wennergren.se>
2016-08-10 15:57:00 +0200

    i think i fixed a bug...

--- install.sh
+++ install.sh
@@ -14,33 +14,14 @@ IGNORE=(
     "setup"
     "scripts"
     "linux"
+    ".config"
 )
 
 DOTFILES_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Sourcing functions for output
+source "$DOTFILES_ROOT/setup/outputs.sh"
 
 echo ''
-
-info () {
-    printf "\r  [ \033[00;34m..\033[0m ] $1\n"
-}
-
-user () {
-    printf "\r  [ \033[0;33m??\033[0m ] $1\n"
-}
-
-success () {
-    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-}
-
-fail () {
-    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-    echo ''
-    exit
-}
-
-noFile () {
-    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-}
 
 ask() {
   # http://djm.me/ask
