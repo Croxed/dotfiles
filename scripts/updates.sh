d09425836e82204c739ede77499894a60d3bd556
Oscar Wennergren <oscar@wennergren.se>
2016-08-14 10:51:59 +0200

    Added missing file in previous commit

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-UPDATES=$(checkupdates | wc -l)
+if type -p checkupdates >/dev/null 2>&1; then
+    UPDATES=$(checkupdates | wc -l)
+elif type -p apt-show-versions >/dev/null 2>&1; then
+    UPDATES=$(apt-show-versions -u | wc -l)
+else
+    UPDATES=0
+fi
 [[ "${UPDATES}" = "0" ]] && exit 0
 
 echo "   ${UPDATES} "
