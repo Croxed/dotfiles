f36794c27c50abf81255f6d9d7ac788272feaa2c
Oscar Wennergren <oscar@wennergren.se>
2017-01-06 19:14:24 +0100

    Argh! About to give up :(

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#
-# Inspired by https://github.com/holman/dotfiles
-#
+
+set -e
+set -u
 
 IGNORE=(
     ".git"
@@ -17,31 +17,7 @@ IGNORE=(
     ".config"
 )
 
-info () {
-    printf "\r  [ \033[00;34m..\033[0m ] $1\n"
-}
-
-user () {
-    printf "\r  [ \033[0;33m??\033[0m ] $1\n"
-}
-
-success () {
-    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-}
-
-fail () {
-    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-    echo ''
-    exit
-}
-
-noFile () {
-    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-}
-
 DOTFILES_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo ''
 
 ask() {
   # http://djm.me/ask
@@ -75,6 +51,67 @@ ask() {
   done
 }
 
+progress()
+{
+  tput civis
+  local -r pid=${1}
+  local -r delay='0.05'
+  local -r before=${2}
+  spinners=(⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏)
+  local temp
+  while kill -0 $pid 2>/dev/null;
+  do
+    for spinner in "${spinners[@]}"
+    do
+      sleep ${delay}
+      printf " \e[97m$spinner\e[39m  $before\r" 2>/dev/null
+    done
+  done
+}
+
+completed(){
+  printf "\033[2K" 2>/dev/null
+  printf " \e[34m\U2714\e[39m $1 [\e[32mSUCCEEDED\e[39m]\n" \
+  2>/dev/null
+  tput cnorm || true
+}
+
+failed(){
+  printf "\033[2K" 2>/dev/null
+  printf " \e[33m\U26A0\e[39m $1 [\e[31mFAILED\e[39m]\n" \
+  2>/dev/null
+  tput cnorm || true
+}
+info(){
+  printf "\033[2K" 2>/dev/null
+  printf " \e[34m\U2753\e[39m $1 [\e[33mINFO\e[39m]\n" \
+  2>/dev/null
+  tput cnorm || true
+}
+testing(){
+  if command -v $1 2&>/dev/null; then
+    echo "true"
+  else
+    echo "false"
+  fi
+}
+
+control_dependencies(){
+  for arg in "$@"; do
+    tempfile="$(mktemp)"
+    testing $arg > ${tempfile} &
+    progress $! "Controlling $arg"
+    exec 3< ${tempfile}
+    read <&3 line
+    if [ "$line" == "true" ]; then
+        completed "Command $arg was found"
+    elif [ "$line" == "false" ]; then
+        failed "Command $arg was not found"
+        exit -1
+    fi
+  done
+}
+
 link_file () {
     local src=$1 dst=$2
 
@@ -83,151 +120,22 @@ link_file () {
 
     if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
     then
-
-        if [ "$overwrite_all" == "false" ] && \
-               [ "$backup_all" == "false" ] && \
-               [ "$skip_all" == "false" ]
+        local currentSrc="$(readlink $dst)"
+        if [ "$currentSrc" == "$src" ]
         then
-
-            local currentSrc="$(readlink $dst)"
-
-            if [ "$currentSrc" == "$src" ]
-            then
-
-                skip=true;
-
-            else
-                user "File already exists: $dst ($(basename "$src")), \
-what do you want to do?\n\
-        [s]kip, [S]kip all, [o]verwrite, \
-[O]verwrite all, [b]ackup, [B]ackup all?"
-
-                read -n 1 action
-
-                case "$action" in
-                    o )
-                        overwrite=true;;
-                    O )
-                        overwrite_all=true;;
-                    b )
-                        backup=true;;
-                    B )
-                        backup_all=true;;
-                    s )
-                        skip=true;;
-                    S )
-                        skip_all=true;;
-                    * )
-                    ;;
-                esac
-
-            fi
-
-        fi
-
-        overwrite=${overwrite:-$overwrite_all}
-        backup=${backup:-$backup_all}
-        skip=${skip:-$skip_all}
-
-        if [ "$overwrite" == "true" ]
-        then
-            rm -rf "$dst"
-            success "removed $dst"
-        fi
-
-        if [ "$backup" == "true" ]
-        then
-            mv "$dst" "${dst}.backup"
-            success "moved $dst to ${dst}.backup"
-        fi
-
-        if [ "$skip" == "true" ]
-        then
-            success "skipped $src"
+            skip=true;
+        else
+          mv "$dst" "${dst}.backup"
         fi
     fi
 
     if [ "$skip" != "true" ]  # "false" or empty
     then
         ln -s "$1" "$2"
-        success "linked $1 to $2"
     fi
-}
-
-getos() {
-    case "$(uname)" in
-        "Linux")   os="Linux" ;;
-        "Darwin")  os="$(sw_vers -productName)" ;;
-        *"BSD" | "DragonFly") os="BSD" ;;
-        "CYGWIN"*) os="Windows" ;;
-        "SunOS") os="Solaris" ;;
-        *) printf "%s\n" "Unknown OS detected: $(uname)"; exit 1 ;;
-    esac
-}
-
-getdistro() {
-    [ "$distro" ] && return
-
-    case "$os" in
-        "Linux" )
-                distro="$(awk -F 'NAME=' '/^NAME=/ {printf $2}' /etc/*ease)"
-                distro="${distro//\"}"
-
-                # Workaround for distros that store the value differently.
-                [ -z "$distro" ] && distro="$(awk -F 'TAILS_PRODUCT_NAME="|"' '/^TAILS_PRODUCT_NAME=/ {printf $2}' /etc/*ease)"
-                [ -z "$distro" ] && distro="$(awk '/BLAG/ {print $1; exit}' /etc/*ease)"
-        ;;
-
-        "Mac OS X")
-            distro="$os"
-        ;;
-
-        "iPhone OS")
-            distro="iOS $(sw_vers -productVersion)"
-        ;;
-
-        "BSD")
-            distro="$(uname -s)"
-            distro="${distro/DragonFly/DragonFlyBSD}"
-
-            # Workaround for PCBSD as uname still displays FreeBSD.
-            [ -f "/etc/pcbsd-lang" ] && distro="PCBSD"
-
-            # Workaround for PacBSD as uname displays FreeBSD.
-            [ -f "/etc/pacbsd-release" ] && distro="PacBSD"
-        ;;
-
-        "Windows")
-            distro="$(wmic os get Caption /value)"
-
-            # Strip crap from the output of wmic
-            distro="${distro/Caption'='}"
-            distro="${distro/Microsoft }"
-        ;;
-
-        "Solaris")
-            distro="$(nawk 'NR==1{gsub(/^ \t]+|[ \t]+$/,""); printf $1 " " $2;}' /etc/release)"
-        ;;
-    esac
-}
-
-find_dependencies () {
-    if [ ! -f "$DOTFILES_ROOT/setup/dependencies-${distro}" ]; then
-        noFile "Could not find file with dependencies for distro ${distro}."
-        return 1
-    else
-        success "Found dependency for $distro"
-        return 0
-    fi
-}
-
-install_packages () {
-    if find_dependencies; then bash "$DOTFILES_ROOT/setup/dependencies-${distro}"; fi
 }
 
 install_dotfiles () {
-    info 'installing dotfiles'
-
     local overwrite_all=false backup_all=false skip_all=false
 
     for src in $(find "$DOTFILES_ROOT" -mindepth 1 -maxdepth 1)
@@ -239,14 +147,6 @@ install_dotfiles () {
         dst="$HOME/$(basename "$src")"
         link_file "$src" "$dst"
     done
-}
-
-install_other () {
-    info 'installing other configs'
-    local overwrite_all=false backup_all=false skip_all=false
-    src="$DOTFILES_ROOT/scripts"
-    dst="$HOME/$(basename "$src")"
-    link_file "$src" "$dst"
 
     mkdir -p "$HOME/.config"
     for src in $(find "$DOTFILES_ROOT/.config" -mindepth 1 -maxdepth 1)
@@ -254,60 +154,23 @@ install_other () {
         dst="$HOME/.config/$(basename "$src")"
         link_file "$src" "$dst"
     done
-
-    ask "Install vim-plug?" Y && info "Installing vim-plug" && curl -fsLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && success "Installed vim-plug"
-    if [[ "$os" == "Linux" ]]; then
-        ask "Make ZSH the default shell?" Y && info "Making ZSH the default shell" && chsh -s "$(which zsh)"; success "Made ZSH default shell"
-    elif [[ "$os" == "Mac OS X" ]]; then
-        ask "Make ZSH the default shell?" Y && info "Making ZSH the default shell" && sudo dscl . -create /Users/$USER UserShell $(brew --prefix)/bin/zsh; success "Made ZSH default shell"
-    fi
 }
 
-os_specific () {
-    if [ -d "$DOTFILES_ROOT/$platform" ]; then
-        for src in $(find "$DOTFILES_ROOT/$platform" -mindepth 1 -maxdepth 1)
-        do
-            if [[ "${IGNORE[@]}" =~ "$(basename $src)" ]]
-            then
-                continue
-            fi
-            dst="$HOME/$(basename "$src")"
-            link_file "$src" "$dst"
-        done
-
-        mkdir -p "$HOME/.config"
-        for src in $(find "$DOTFILES_ROOT/$platform/.config" -mindepth 1 -maxdepth 1)
-        do
-            dst="$HOME/.config/$(basename "$src")"
-            link_file "$src" "$dst"
-        done
-    fi
+dotfiles(){
+  tempfile="$(mktemp)"
+  install_dotfiles > ${tempfile} &
+  progress $! "Installing dotfiles.."
+  exec 3< ${tempfile}
+  read <&3 line
+  if [ "$line" == "true" || "$line" == "" ]; then
+      completed "Done installing dotfiles"
+  elif [ "$line" == "false" ]; then
+      failed "Dotfiles not installed"
+      exit -1
+  fi
 }
 
-install_bin () {
-    info 'installing bin'
-    local overwrite_all=false backup_all=false skip_all=false
-    src="$DOTFILES_ROOT/bin"
-    dst="$HOME/$(basename "$src")"
-    link_file "$src" "$dst"
-}
 
-getos
-getdistro
-ask "Install packages?" Y && install_packages
-echo ''
-ask "Install dotfiles?" Y && install_dotfiles
-echo ''
-ask "Install bin?" Y && install_bin
-echo ''
-ask "Install other scripts and configs?" Y && install_other
-echo ''
-if [[ "$os" == "Linux" ]]; then
-    ask "Install configs for $distro?" Y && os_specific
-fi
-
-echo ''
-if [[ "$distro" == 'Mac OS X' ]]; then
-  ask "Install sensible defaults for Mac OS X?" Y && bash "$DOTFILES_ROOT/setup/macos"
-  ask "Install QuickLook plugins?" Y && bash "$DOTFILES_ROOT/setup/qlInstall"
-fi
+control_dependencies "tmux" "nvim" "git" "zsh"
+dotfiles
+printf " All processes are successfully completed \U1F389\n"
