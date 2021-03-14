ee5754c18b36017b4f8f4f7b894948717e312a0d
Oscar Wennergren <oscar.wennergren@gamesysgroup.com>
2021-03-14 21:33:41 +0100

    Add better version manager for php

---
+++ .zsh/.zsh-config/plugins/phpbrew.zsh
@@ -0,0 +1,9 @@
+#! /usr/bin/env
+
+if ! command -v phpbrew; then
+    mkdir -p "$HOME"/bin.local "$HOME"/.zsh-settings
+    curl -LsfS "https://github.com/phpbrew/phpbrew/raw/master/phpbrew" -o "$HOME"/bin.local/phpbrew
+    chmod +x "$HOME"/bin.local/phpbrew
+    phpbrew -q init
+    ln -sfn "$HOME"/.phpbrew/bashrc "$HOME"/.zsh-settings/phpbrew.zsh
+fi
