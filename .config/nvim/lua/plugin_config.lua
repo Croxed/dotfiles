48a8e1ea01da99846061c17e7d4f0e01e1180d4d
Oscar Wennergren <oscar.wennergren@gamesysgroup.com>
2021-06-22 14:37:43 +0200

    Remove some plugins

--- .config/nvim/lua/plugin_config.lua
+++ .config/nvim/lua/plugin_config.lua
@@ -95,12 +95,25 @@ vim.g.nord_uniform_status_lines = 1
 vim.g.nord_uniform_diff_background = 1
 vim.g.nord_cursor_line_number_background = 1
 
+local disable_distribution_plugins= function()
+  vim.g.loaded_gzip              = 1
+  vim.g.loaded_tar               = 1
+  vim.g.loaded_tarPlugin         = 1
+  vim.g.loaded_zip               = 1
+  vim.g.loaded_zipPlugin         = 1
+  vim.g.loaded_getscript         = 1
+  vim.g.loaded_getscriptPlugin   = 1
+  vim.g.loaded_vimball           = 1
+  vim.g.loaded_vimballPlugin     = 1
+  vim.g.loaded_matchit           = 1
+  vim.g.loaded_matchparen        = 1
+  vim.g.loaded_2html_plugin      = 1
+  vim.g.loaded_logiPat           = 1
+  vim.g.loaded_rrhelper          = 1
+  vim.g.loaded_netrw             = 1
+  vim.g.loaded_netrwPlugin       = 1
+  vim.g.loaded_netrwSettings     = 1
+  vim.g.loaded_netrwFileHandlers = 1
+end
 
-vim.g.loaded_gzip = false
-vim.g.loaded_matchit = false
-vim.g.loaded_netrwPlugin = false
-vim.g.loaded_tarPlugin = false
-vim.g.loaded_zipPlugin = false
-vim.g.loaded_man = false
-vim.g.loaded_2html_plugin = false
-vim.g.loaded_remote_plugins = false
+disable_distribution_plugins()
