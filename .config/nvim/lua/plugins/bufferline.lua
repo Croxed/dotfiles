a36ccbdd1e16ff47e1353bc23e85589fb8677cd7
Oscar Wennergren <oscar.wennergren@gamesysgroup.com>
2021-08-24 14:32:27 +0200

    Remove deprecated option for bufferline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -14,7 +14,6 @@ local accent3 = "#EBCB8B" -- Not saved
 bufferline.setup({
 	options = {
 		numbers = "none",
-		mappings = true,
 		-- NOTE: this plugin is designed with this icon in mind,
 		-- and so changing this is NOT recommended, this is intended
 		-- as an escape hatch for people who cannot bear it for whatever reason
