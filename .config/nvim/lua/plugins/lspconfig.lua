f54d4ebc803f3105567cd36b225d8bdd8ba3e243
Oscar Wennergren <oscar.wennergren@gamesysgroup.com>
2022-04-07 21:57:18 +0200

    Remove some old code

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,6 @@
 local present, lsp_install = pcall(require, 'nvim-lsp-installer.servers')
 local present_2, lsp_installer = pcall(require, 'nvim-lsp-installer')
+local present_2, lsp_installer_servers = pcall(require, 'nvim-lsp-installer.servers')
 
 if not present or not present_2 then
 	return
@@ -8,12 +9,12 @@ end
 -- LSP
 require("lsp")
 local function install_server(server)
-	local ok, server_cmd = lsp_install.get_server(server)
-	if ok then
-		if not server_cmd:is_installed() then
-			server_cmd:install()
-		end
-	end
+  local ok, server_conf = lsp_installer_servers.get_server(server)
+  if ok then
+    if not server_conf:is_installed() then
+      server_conf:install()
+    end
+  end
 end
 
 local function install_missing_servers()
@@ -24,11 +25,43 @@ local function install_missing_servers()
 	end
 end
 
+local language_servers = {
+	sumneko_lua = {
+		config = function(opts)
+		opts = vim.tbl_deep_extend("force", {
+			settings = {
+			Lua = {
+				runtime = {version = 'LuaJIT', path = vim.split(package.path, ';')},
+				diagnostics = {globals = {'vim'}},
+				workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false
+				},
+				telemetry = {enable = false}
+			}
+			}
+
+		}, opts)
+		return opts
+		end
+	},
+	jsonls = {
+		config = function(opts)
+		opts = vim.tbl_deep_extend("force", {
+			settings = {json = {schemas = require('schemastore').json.schemas()}}
+		}, opts)
+		return opts
+		end
+	}
+}
+
+
 lsp_installer.on_server_ready(function(server)
-  local opts = {
-    on_attach = require("lsp").common_on_attach,
-    capabilities = require("lsp").get_capabilities(),
-  }
+  local opts = {capabilities = require('lsp').get_capabilities(), on_attach = require("lsp").common_on_attach}
+  if language_servers[server.name] then
+    opts = language_servers[server.name].config(opts)
+  end
+  server:setup(opts)
 
   -- (optional) Customize the options passed to the server
   -- if server.name == "tsserver" then
