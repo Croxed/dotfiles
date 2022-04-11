59b5ac36477d925156a01f13b056ed4936483744
Oscar Wennergren <oscar.wennergren@gamesysgroup.com>
2022-04-11 22:29:53 +0200

    Maybe detect WSLG

--- .zsh/.zsh-config/plugins/wsl2.zsh
+++ .zsh/.zsh-config/plugins/wsl2.zsh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
-
-if command -v "wsl.exe" &>/dev/null && ! command -v "wlsg.exe" &>/dev/null ; then
+if [ -f "/mnt/c/Windows/system32/reg.exe" ]; then
+  build_version="$(/mnt/c/Windows/system32/reg.exe query "HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion" /v CurrentBuild | tail -n 2 | head -n 1 | sed -e 's|\r||g' | awk '{print $3}')"
+  if [ "$build_version" -lt 20000 ]; then
     export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
     export DISPLAY=$WSL_HOST:0.0
 
     export LIBGL_ALWAYS_INDIRECT=1
+  fi
 fi
