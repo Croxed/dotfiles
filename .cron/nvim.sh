4a2b0c7930f7d7266076c6329d96af91d0a5a0a2
Oscar Wennergren <oscar.wennergren@gamesysgroup.com>
2022-10-12 08:48:41 +0200

    Update nvim cron download script

--- .cron/nvim.sh
+++ .cron/nvim.sh
@@ -4,8 +4,19 @@ set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-# shellcheck disable=1091
-. "$DIR"/utils.sh
+
+get_latest_release() {
+	local user repo type
+	user="$1"
+	shift
+	repo="$1"
+	shift
+	type="$1"
+	shift
+	curl -s "https://api.github.com/repos/$user/$repo/releases" |
+    jq -rc "[.[] | select(.prerelease == false) | [.name, (.assets[] | select(.browser_download_url | contains(\"$type\")) | .browser_download_url)]][0] | .[]"
+}
+
 
 neovim_dir="$DIR"/.neovim
 
@@ -28,7 +39,6 @@ main() {
 	response="$(get_latest_release neovim neovim "$type")"
 	version="$(head -n1 <<<"$response")"
 	url="$(grep -E 'tar.gz$' <<<"$response")"
-
 	rm -rf "$neovim_dir" && mkdir -p "$neovim_dir"
 	cd "$neovim_dir" || exit 1
 	curl -fsSL "$url" | tar xz --strip-components=1
