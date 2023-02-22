1cb20598caa8522811faa56537494d3afb42153d
Oscar Wennergren <oscar.wennergren@gamesysgroup.com>
2023-02-22 10:20:23 +0100

    More improvements

--- .config/nvim/lua/config/plugins/neo-tree.lua
+++ .config/nvim/lua/config/plugins/neo-tree.lua
@@ -1,14 +1,83 @@
-return {
-  "nvim-neo-tree/neo-tree.nvim",
-  cmd = { "NeoTree", "NeoTreeShowToggle" },
-  config = function()
-    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+local root_patterns = { ".git", "lua", "pom.xml", "build.gradle", "pyproject.toml", "go.mod" }
+local function get_root()
+  ---@type string?
+  local path = vim.api.nvim_buf_get_name(0)
+  path = path ~= "" and vim.loop.fs_realpath(path) or nil
+  ---@type string[]
+  local roots = {}
+  if path then
+    for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+      local workspace = client.config.workspace_folders
+      local paths = workspace and vim.tbl_map(function(ws)
+        return vim.uri_to_fname(ws.uri)
+      end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
+      for _, p in ipairs(paths) do
+        local r = vim.loop.fs_realpath(p)
+        if path:find(r, 1, true) then
+          roots[#roots + 1] = r
+        end
+      end
+    end
+  end
+  table.sort(roots, function(a, b)
+    return #a > #b
+  end)
+  ---@type string?
+  local root = roots[1]
+  if not root then
+    path = path and vim.fs.dirname(path) or vim.loop.cwd()
+    ---@type string?
+    root = vim.fs.find(root_patterns, { path = path, upward = true })[1]
+    root = root and vim.fs.dirname(root) or vim.loop.cwd()
+  end
+  ---@cast root string
+  return root
+end
 
-    require("neo-tree").setup({
-      filesystem = {
-        follow_current_file = true,
-        hijack_netrw_behavior = "open_current",
-      },
-    })
+local M = {
+  "nvim-neo-tree/neo-tree.nvim",
+  cmd = "Neotree",
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = get_root() })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+  },
+  deactivate = function()
+    vim.cmd([[Neotree close]])
   end,
+  init = function()
+    vim.g.neo_tree_remove_legacy_commands = 1
+    if vim.fn.argc() == 1 then
+      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == "directory" then
+        require("neo-tree")
+      end
+    end
+  end,
+  opts = {
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = true,
+    },
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+      },
+    },
+  },
 }
+
+return M
