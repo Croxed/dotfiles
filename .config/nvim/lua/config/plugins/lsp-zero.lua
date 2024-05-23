ad9bc23b240844d4a443446bf2d1d77c546c2a77
Oscar Wennergren <oscar.wennergren@gamesysgroup.com>
2024-05-23 21:31:38 +0200

    Update lazy-lock and change autocompletion key

--- .config/nvim/lua/config/plugins/lsp-zero.lua
+++ .config/nvim/lua/config/plugins/lsp-zero.lua
@@ -39,7 +39,7 @@ function M.config()
   local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
   })
   lsp.setup_nvim_cmp({
