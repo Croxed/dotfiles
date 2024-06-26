3f7525c03446115c56280b0cbb31ab13a51dffa1
Oscar Wennergren <oscar.wennergren@ballys.com>
2024-06-26 07:52:07 +0200

    Fix gitsigns

--- .config/nvim/lua/config/plugins/gitsigns.lua
+++ .config/nvim/lua/config/plugins/gitsigns.lua
@@ -1,90 +1,51 @@
-local M = {
+
+return {
   "lewis6991/gitsigns.nvim",
   event = "BufReadPre",
-}
-
-function M.config()
-  require("gitsigns").setup({
+  opts = {
     signs = {
-      add = { hl = "GitSignsAdd", text = "▍", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-      change = {
-        hl = "GitSignsChange",
-        text = "▍",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-      delete = {
-        hl = "GitSignsDelete",
-        text = "▸",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      topdelete = {
-        hl = "GitSignsDelete",
-        text = "▾",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      changedelete = {
-        hl = "GitSignsChange",
-        text = "▍",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-      untracked = { hl = "GitSignsAdd", text = "▍", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
     },
-    on_attach = function(bufnr)
+    on_attach = function(buffer)
       local gs = package.loaded.gitsigns
 
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        if type(opts) == "string" then
-          opts = { desc = opts }
-        end
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
       end
 
-      -- Navigation
+      -- stylua: ignore start
       map("n", "]h", function()
         if vim.wo.diff then
-          return "]h"
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gs.nav_hunk("next")
         end
-        vim.schedule(function()
-          gs.next_hunk()
-        end)
-        return "<Ignore>"
-      end, { expr = true, desc = "Next Hunk" })
-
+      end, "Next Hunk")
       map("n", "[h", function()
         if vim.wo.diff then
-          return "[h"
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gs.nav_hunk("prev")
         end
-        vim.schedule(function()
-          gs.prev_hunk()
-        end)
-        return "<Ignore>"
-      end, { expr = true, desc = "Prev Hunk" })
-
-      -- Actions
+      end, "Prev Hunk")
+      map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
+      map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
       map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
       map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
       map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
       map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
       map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-      map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
-      map("n", "<leader>ghb", function()
-        gs.blame_line({ full = true })
-      end, "Blame Line")
+      map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+      map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
       map("n", "<leader>ghd", gs.diffthis, "Diff This")
-      map("n", "<leader>ghD", function()
-        gs.diffthis("~")
-      end, "Diff This ~")
-
-      -- Text object
+      map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
     end,
-  })
-end
-
-return M
+  },
+}
