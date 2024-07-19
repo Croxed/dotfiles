888df5b7b2e366019ac727b2ca417cd0f881afde
Oscar Wennergren <oscar.wennergren@gamesysgroup.com>
2024-07-19 11:45:21 +0200

    Migrate to LazyVim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,27 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
 local cmd = vim.cmd
 local opt = {}
 
-vim.g.mapleader = " "
-
 -- toggle numbers
 vim.keymap.set("n", "<leader>n", [[ <Cmd> set nu!<CR>]], opt)
-
--- Truezen.nvim
-vim.keymap.set("n", "<leader>tz", [[<Cmd>TZAtaraxis<CR>]], opt) --ataraxis
-
--- Commenter Keybinding
-vim.keymap.set("n", "<leader>/", vim.cmd.CommentToggle)
-vim.keymap.set("v", "<leader>/", vim.cmd.CommentToggle)
-
--- NvimTree
-vim.keymap.set("n", "<leader>op", vim.cmd.NvimTree)
-
 vim.keymap.set("i", "jk", "<esc>") --jk to exit
 vim.keymap.set("c", "jk", "<C-C>")
 vim.keymap.set("n", ";", ":") --semicolon to enter command mode
 vim.keymap.set("n", "j", "gj") --move by visual line not actual line
 vim.keymap.set("n", "k", "gk")
-vim.keymap.set("n", "<leader>fP", function() vim.cmd.e('~/.config/nvim/init.lua') end)
 
 vim.keymap.set("n", "<c-k>", function() vim.cmd.wincmd('k') end) --ctrlhjkl to navigate splits
 vim.keymap.set("n", "<c-j>", function() vim.cmd.wincmd('j') end)
@@ -38,31 +28,11 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- compe stuff
-local t = function(str)
-	return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-	local col = vim.fn.col(".") - 1
-	if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-		return true
-	else
-		return false
-	end
-end
-
-
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
 -- move between tabs
 vim.keymap.set("n", "<TAB>", vim.cmd.BufferLineCycleNext)
 vim.keymap.set("n", "<S-TAB>", vim.cmd.BufferLineCyclePrev)
 
---nvimTree
-vim.keymap.set("n", "<Leader>o", vim.cmd.NeoTreeShowToggle)
 --lsp
-
 vim.keymap.set("n", "<Leader>gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "<Leader>gD", vim.lsp.buf.declaration)
 vim.keymap.set("n", "<Leader>gr", vim.lsp.buf.references)
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,44 +1,50 @@
--- bootstrap from github
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--single-branch",
-        "git@github.com:folke/lazy.nvim.git",
-        lazypath,
-    })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-vim.opt.runtimepath:prepend(lazypath)
--- load lazy
-require("lazy").setup("config.plugins", {
-  defaults = { lazy = true },
-  install = { colorscheme = { "nord", } },
-  checker = { enabled = true },
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "nord", "tokyonight" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
+      -- disable some rtp plugins
       disabled_plugins = {
-        "netrw",
-        "netrwPlugin",
-        "netrwSettings",
-        "netrwFileHandlers",
         "gzip",
-        "zip",
-        "zipPlugin",
-        "tar",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
         "tarPlugin",
-        "getscript",
-        "getscriptPlugin",
-        "vimball",
-        "vimballPlugin",
-        "2html_plugin",
-        "logipat",
-        "rrhelper",
-        "spellfile_plugin",
-        "matchit",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
       },
     },
   },
 })
-vim.keymap.set("n", "<leader>l", "<cmd>:Lazy<cr>")
